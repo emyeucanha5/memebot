8a0296debd27c3a9c09f2287e5ff3510379769d0
thelazyant164 <lythienan164@gmail.com>
2022-08-07 23:55:03 +1000

    [W] - WIP: adding database integration

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+import pymongo
+from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 import discord
 import requests
+from requests.exceptions import HTTPError
 import json
 
 load_dotenv()
@@ -10,64 +13,93 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 COMMAND = os.getenv('DISCORD_COMMAND_ROOT')
 SUBREDDIT_COMMAND = os.getenv('DISCORD_COMMAND_SUBREDDIT')
+DB_SAVE_COMMAND = os.getenv('DISCORD_COMMAND_DB_SAVE')
+DB_LOAD_COMMAND = os.getenv('DISCORD_COMMAND_DB_LOAD')
+API_ROOT = os.getenv('MEME_API_ROOT')
+MONGODB_URI = os.getenv('MONGODB_URI')
+DB = os.getenv('DB')
+COLLECTION = os.getenv('COLLECTION')
 client = discord.Client()
 
+collection = MongoClient(MONGODB_URI)[DB][COLLECTION]
 
-def getRandomMemes(count):
+
+def get_random_memes(count):
     try:
-        response = requests.get(f"https://meme-api.herokuapp.com/gimme/{count}")
-    except error:
+        response = requests.get(f"{API_ROOT}{count}")
+    except HTTPError as error:
         return error
     json_data = json.loads(response.text)
     return json_data
 
-def getRandomMemeFromSubreddits(subReddit, count):
+
+def get_random_meme_from_subreddits(subReddit, count):
     try:
-        response = requests.get(f"https://meme-api.herokuapp.com/gimme/{subReddit}/{count}")
-    except error:
+        response = requests.get(f"{API_ROOT}{subReddit}/{count}")
+    except HTTPError as error:
         return error
     if(response.status_code == 404):
         return 'fail'
     json_data = json.loads(response.text)
     return json_data
 
-def checkLinkDie(link):
+
+def check_link_die(link):
     try:
         response = requests.get(link)
-    except :
+    except:
         return False
     if(response.status_code == 404):
         return False
     return True
 
-    
+
 @client.event
 async def on_message(message):
     if (message.content == COMMAND):
-        randomMeme = getRandomMemes(1)['memes'][0]
-        while(checkLinkDie(randomMeme['url']) == False):
-            randomMeme = getRandomMemes(1)['memes'][0]
-            
-        await message.channel.send(randomMeme['url'])
-        
+        random_meme = get_random_memes(1)['memes'][0]
+        while(check_link_die(random_meme['url']) == False):
+            random_meme = get_random_memes(1)['memes'][0]
+        await message.channel.send(random_meme['url'])
+
     if(message.content.startswith(COMMAND + ' ') and len(message.content) <= len(COMMAND) + 2 and message.content[9].isnumeric()):
-       randomMemes = getRandomMemes(int(message.content[9]))['memes']
-       randomMemesImageLink = [e['url'] for e in randomMemes]
-       for link in randomMemesImageLink:
-           if(checkLinkDie(link)==False):
-            continue;
-           else:
-            await message.channel.send(link)
-            
+        random_memes = get_random_memes(int(message.content[9]))['memes']
+        random_memes_image_link = [e['url'] for e in random_memes]
+        for link in random_memes_image_link:
+            if(check_link_die(link) == False):
+                continue
+            else:
+                await message.channel.send(link)
+
     if(message.content.startswith(SUBREDDIT_COMMAND)):
         subreddit = message.content[len(SUBREDDIT_COMMAND)+1:]
-        randomMeme = getRandomMemeFromSubreddits(subreddit,1)
-        if(isinstance(randomMeme, str)):
-            await message.channel.send('Sub reddit is not found')
-            return
-        randomMemeSubReddit = randomMeme['memes'][0];
-        while(checkLinkDie(randomMemeSubReddit['url']) == False):
-            randomrandomMemeSubRedditMeme = getRandomMemeFromSubreddits(subreddit,1)['memes'][0]
-        await message.channel.send(randomMemeSubReddit['url'])   
-    
+        random_meme = get_random_meme_from_subreddits(subreddit, 1)
+        try:
+            if(random_meme["code"] == 400):
+                await message.channel.send('Subreddit not found')
+                return
+        except KeyError:
+            random_meme_subreddit = random_meme['memes'][0]
+            while(check_link_die(random_meme_subreddit['url']) == False):
+                random_meme = get_random_meme_from_subreddits(subreddit, 1)
+                random_meme_subreddit = random_meme['memes'][0]
+            await message.channel.send(random_meme_subreddit['url'])
+
+    # TODO: error handling for ineligible meme to save
+    if(message.content.startswith(DB_SAVE_COMMAND)):
+        saved_name = message.content[len(DB_SAVE_COMMAND)+1:]
+        saved_meme = {
+            "name": saved_name,
+            # TODO: extract previous message sent by bot & extract meme url
+            "url": "https://link_to_meme_img"
+        }
+        collection.insert_one(saved_meme)
+        await message.channel.send("Saved to database!")
+
+    # TODO: error handling when no match found
+    if(message.content.startswith(DB_LOAD_COMMAND)):
+        lookup_name = message.content[len(DB_LOAD_COMMAND)+1:]
+        found_meme = collection.find_one({"name": lookup_name})
+        await message.channel.send(found_meme["url"])
+
 client.run(TOKEN)
